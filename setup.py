c008b4624b717ac3b7a5ce2a4ca5685b4d2c24ec
Mikel Larreategi <mlarreategi@codesyntax.com>
2015-11-17 14:59:08 +0100

    move to RST

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0'
 setup(name='cs.p5migrator',
       version=version,
       description="Migrator of CS Sites to P5",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
